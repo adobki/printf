c852e93fed3b0bdd3c76f0676c295831bf8d62d9
Donald Ajaps <95923018+adobki@users.noreply.github.com>
2023-09-20 09:04:16 +0100

    Update _printf.c
    * Change order of functions in source file
    * Apply minor fixes.

--- _printf.c
+++ _printf.c
@@ -1,56 +1,6 @@
 #include "main.h"
 
 /**
- * print_char - Prints a single character.
- *
- * @c: String to be printed.
- *
- * Return: 0 or 1 on SUCCESS,
- *        -1 on ERROR.
- */
-int print_char(char c)
-{
-	int ret = 0;
-	char *str;
-
-	if (c)
-		ret = write(STDOUT_FILENO, &c, 1);
-	else
-	{
-		str = "(null)";
-		write(STDOUT_FILENO, str++, 1);
-		write(STDOUT_FILENO, str++, 1);
-		write(STDOUT_FILENO, str++, 1);
-		write(STDOUT_FILENO, str++, 1);
-		write(STDOUT_FILENO, str++, 1);
-		write(STDOUT_FILENO, str++, 1);
-		ret = 6;
-	}
-
-	return (ret);
-}
-
-/**
- * print_str - Prints a string without formatting.
- *
- * @str: String to be printed.
- *
- * Return: Number of characters printed.
- */
-int print_str(char *str)
-{
-	int i = 0;
-
-	if (!str || !*str)
-		i = print_char(0);
-	else
-		while (*str)
-			print_char(*str++), i++;
-
-	return (i);
-}
-
-/**
  * _printf - Program entry point.
  *
  * @format: Format string used for printing.
@@ -74,19 +24,61 @@ int _printf(const char *format, ...)
 			if (format[i])
 			{
 				if (format[i] == '%')
+					print_char('%'), printed++, i++;
+				else if (format[i] == 'c')
+					print_char(va_arg(vargp, int)), printed++, i++;
+				else if (format[i] == 's')
+					printed += print_str((char *)va_arg(vargp, char *)), i++;
+				else
 					print_char('%'), printed++;
-				if (format[i] == 'c')
-					print_char(va_arg(vargp, int)), printed++;
-				if (format[i] == 's')
-					printed += print_str((char *)va_arg(vargp, char *));
 			}
 		}
 		else
-			print_char(format[i]), printed++;
-
-		i++;
+			print_char(format[i]), printed++, i++;
 	}
 	va_end(vargp);
 
 	return (printed);
+}
+
+/**
+ * print_char - Prints a single character.
+ *
+ * @c: String to be printed.
+ *
+ * Return: 0 or 1 on SUCCESS,
+ *        -1 on ERROR.
+ */
+int print_char(char c)
+{
+	return (write(STDOUT_FILENO, &c, 1));
+}
+
+/**
+ * print_str - Prints a string without formatting.
+ *
+ * @str: String to be printed.
+ *
+ * Return: Number of characters printed.
+ */
+int print_str(char *str)
+{
+	int i = 0;
+
+	if (!str)
+	{
+		str = "(null)";
+		write(STDOUT_FILENO, str++, 1);
+		write(STDOUT_FILENO, str++, 1);
+		write(STDOUT_FILENO, str++, 1);
+		write(STDOUT_FILENO, str++, 1);
+		write(STDOUT_FILENO, str++, 1);
+		write(STDOUT_FILENO, str++, 1);
+		i = 6;
+	}
+	else
+		while (*str)
+			print_char(*str++), i++;
+
+	return (i);
 }
